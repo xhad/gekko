b805432af2bb5520f21865375300eec50366ee15
Mike van Rossum <mike@askmike.org>
2014-03-03 14:35:53 +0100

    fixed first error in #220

--- core/exchangeChecker.js
+++ core/exchangeChecker.js
@@ -38,7 +38,7 @@ Checker.prototype.cantMonitor = function(conf) {
   });
 
   if(!pair)
-    return 'Gekko only supports the currency/assets pairs [' + exchange.pairs.join('], [') + ']';
+    return 'Gekko only supports the currency/assets pairs [' + _.pluck(exchange.markets, 'pair').join('], [') + ']';
 
   // everyting okay
   return false;
