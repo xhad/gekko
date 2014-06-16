17ea58394c1bd811750ae440d00244713401c5f8
Morgan McMurry <wmbandgeek88@gmail.com>
2014-06-15 20:37:52 -0500

    fixed minor typo

--- gekko.js
+++ gekko.js
@@ -34,7 +34,7 @@ if(util.getArgument('v')) {
 // make sure the current node version is recent enough
 if(!util.recentNode())
   util.die([
-    'Your local version of nodejs is to old. ',
+    'Your local version of nodejs is to0 old. ',
     'You have ',
     process.version,
     ' and you need atleast ',
